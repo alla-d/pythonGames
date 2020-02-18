3d3de24bc5cf4d681faaac66bf687fe90c81aebf
alla-d <aviator.nr1@gmail.com>
2020-02-18 20:43:35 +0100

    Small changes in code

--- games/guessTheNumber.py
+++ games/guessTheNumber.py
@@ -23,9 +23,9 @@ for guessesTaken in range(6):
         break
 
 if guess == number:
-        guessesTaken = str(guessesTaken + 1)
-        print('Good job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!')
+    guessesTaken = str(guessesTaken + 1)
+    print('Good job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!')
 
 if guess != number:
-        number = str(number)
-        print('Nope. The number I was thinking of was ' + number)
+    number = str(number)
+    print('Nope. The number I was thinking of was ' + number)
